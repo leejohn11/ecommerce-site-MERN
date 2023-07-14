4580e57fb3a42b013635109928ceefd8e01d8e0b
Lee <leahkjohn@gmail.com>
2023-07-14 13:00:27 +0530

    commit

---
+++ controllers/authController.js
@@ -0,0 +1,15 @@
+export const registerController= async(req,res)=>{
+    try {
+        const {name,email,password,phone,addess} =req.body
+        if(!name){
+            return res.send({error:'Name is req'})``
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message: "error reg",
+            error
+        })
+    }
+};
